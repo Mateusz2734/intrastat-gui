8e3def6954eab6edbf7fb65290fff0a5e57703da
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-08-01 23:09:22 +0200

    fix: Add some constants

--- widgets/settings/logic.py
+++ widgets/settings/logic.py
@@ -2,19 +2,28 @@ import logging as log
 import json
 from os.path import exists as file_exists
 
+DEFAULT_DATA = {
+    "invoice": {"db": None},
+    "intrastat": {"db1": None, "db2": None},
+    "sampa": {"db": None},
+    "desha": {"db": None},
+}
+
+SETTINGS_FILE = "C:/Skrypty/Pomocnik/settings.json"
+
 
 def create_settings():
-    default_data = {"invoice": {"db": None}, "intrastat": {"db1": None, "db2": None}, "sampa": {"db": None}}
-    if not file_exists("C:/Skrypty/Pomocnik/settings.json"):
+    if not file_exists(SETTINGS_FILE):
         try:
-            with open('C:/Skrypty/Pomocnik/settings.json', 'w') as file:
-                file.write(json.dumps(default_data, indent=4, sort_keys=True))
+            with open(SETTINGS_FILE, "w") as file:
+                file.write(json.dumps(DEFAULT_DATA, indent=4, sort_keys=True))
         except Exception as e:
             log.error(f"{__name__} :: {str(e)}")
 
+
 def read_settings():
     try:
-        with open('C:/Skrypty/Pomocnik/settings.json', 'r') as file:
+        with open(SETTINGS_FILE, "r") as file:
             json_settings = json.load(file)
         return json_settings
     except Exception as e:
